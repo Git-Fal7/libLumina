5bc76169f89fa41ada2f7a53224a3c85d9457543
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-08-17 16:50:13 -0400

    update

--- LuminaOS-Gentoo.cpp
+++ LuminaOS-Gentoo.cpp
@@ -4,7 +4,7 @@
 //  Available under the 3-clause BSD license
 //  See the LICENSE file for full details
 //===========================================
-#ifdef __linux__
+
 #include <QDebug>
 #include "LuminaOS.h"
 #include <unistd.h>
@@ -306,4 +306,3 @@ QStringList LOS::DiskUsage(){ //Returns: List of current read/write stats for ea
 
   return out;
 }
-#endif
